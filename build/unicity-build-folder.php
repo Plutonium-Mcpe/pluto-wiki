2a312530ecb93a81a98148798f85c8f79245b030
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-01-09 00:20:04 +0100

    Amélioration détection index catégorie

--- build/unicity-build-folder.php
+++ build/unicity-build-folder.php
@@ -5,29 +5,44 @@ require __DIR__ . "/build-base.php";
 $cache = [];
 
 if (count($argv) !== 2) {
-    die("Fournir un chemin valide");
+	die("Fournir un chemin valide");
 }
 
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($argv[1], FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_PATHNAME)) as $file) {
-    if (substr($file, -3) !== ".md") {
-        continue;
-    }
-    $contents = file_get_contents($file);
-    print_r("Contenu du fichier $file récupéré\n");
-    $filename = getFilename($file);
-    $category = substr($filename, strlen($filename) - 9) === "_category";
-    $name = getId($contents);
-    if ($category) {
-        $categName = substr($filename, strlen($filename) - 9);
-        if (isset($cache[$categName][$name . "_category"])) {
-            throw new RuntimeException("L'index pour la catégorie $categName existe déjà");
-        }
-    } else {
-        $categ = getCategory($contents);
-        if (isset($cache[$categ][$name])) {
-            throw new RuntimeException("$name pour la catégorie $categ existe déjà");
-        }
-        $cache[$categ][$name] = $file;
-    }
-    print_r("Fichier $file valide\n");
+	if (substr($file, -3) !== ".md") {
+		continue;
+	}
+	$contents = file_get_contents($file);
+	print_r("Contenu du fichier $file récupéré\n");
+	$filename = getFilename($file);
+	$category = substr($filename, strlen($filename) - 9) === "_category";
+	$name = getId($contents);
+	if ($category) {
+		$categName = substr($filename, strlen($filename) - 9);
+		if (isset($cache[$categName][$name . "_category"])) {
+			throw new RuntimeException("L'index pour la catégorie $categName existe déjà");
+		}
+	} else {
+		$categ = getCategory($contents);
+		if (isset($cache[$categ][$name])) {
+			throw new RuntimeException("$name pour la catégorie $categ existe déjà");
+		}
+		$cache[$categ][$name] = $file;
+	}
+	print_r("Fichier $file valide\n");
+}
+
+foreach ($cache as $categ => $art) {
+	$asIndex = false;
+	foreach ($art as $name => $file) {
+		$path = $file;
+		$path = str_replace($name . ".md", "", $path);
+		if (file_exists($path . $categ . "_category.md")) {
+			$asIndex = true;
+			continue;
+		}
+	}
+	if (!$asIndex) {
+		throw new RuntimeException("La catégorie $categ n'as pas d'index");
+	}
 }
