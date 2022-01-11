10101af1f10109def25319b7825683c10e4343f4
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-01-11 09:13:24 +0100

    Amélioration génération craft

--- build/build-custom-craft.php
+++ build/build-custom-craft.php
@@ -7,27 +7,46 @@ require __DIR__ . "/vendor/autoload.php";
 
 $recipePath = dirname(__DIR__) . "/static/data/custom_recipe.json";
 if (!is_file($recipePath)) {
-    throw new RuntimeException("custom_recipe.json introuvable");
+	throw new RuntimeException("custom_recipe.json introuvable");
 }
 $recipes = json_decode(file_get_contents($recipePath), true);
 
 @mkdir(dirname(__DIR__) . "/static/craft");
 foreach ($recipes as $recipe) {
-    $craft = new Craft(1);
-    $base = 0;
-    $j = 0;
-    for ($i=1; $i <= 9 ; $i++) {
-        $itemName = $recipe["input"][$recipe["shape"][$j][$base]]["name"];
-        if ($base > 2) {
-            $base = 0;
-            $j++;
-        }
-        $item = new Item(dirname(__DIR__) . "/static/plutonium/textures/" . $itemName . ".png");
-        $craft->addItem($item, $i);
-    }
-    $outputname = $recipe["output"][0]["name"];
-    $craft->addItem(new Item(dirname(__DIR__) . "/static/plutonium/textures/" . $outputname . ".png"), Craft::CRAFT_SLOT_RESULT);
-    
-    $craftname = "craft_" . explode("/", $outputname)[array_key_last(explode("/", $outputname))];
-    $craft->export(dirname(__DIR__) . "/static/craft/" . $craftname . ".png");
+	$craft = new Craft(1);
+	$base = 0;
+	$j = 0;
+	for ($i=1; $i <= 9 ; $i++) {
+		if ($base > 2) {
+			$base = 0;
+			$j++;
+		}
+		$shape = $recipe["shape"][$base][$j];
+		if ($shape === " ") {
+			$base++;
+			continue;
+		}
+		if (!isset($recipe["input"][$shape]["name"])) {
+			throw new RuntimeException("Invalide shape donné: $shape");
+		}
+		$itemName = $recipe["input"][$shape]["name"];
+		$itemfolder = $recipe["input"][$recipe["shape"][$base][$j]]["folder"];
+		$itempath = dirname(__DIR__) . "/static/$itemfolder/textures/$itemName.png";
+		if (!is_file($itempath)) {
+			throw new RuntimeException("Texture: $itemName.png introuvable dans le dossier $itemfolder/textures");
+		}
+		$item = new Item($itempath);
+		$craft->addItem($item, $i);
+		$base++;
+	}
+	$outputname = $recipe["output"][0]["name"];
+	$outputfolder = $recipe["output"][0]["folder"];
+	$outputpath = dirname(__DIR__) . "/static/$outputfolder/textures/$outputname.png";
+	if (!is_file($outputpath)) {
+		throw new RuntimeException("Texture: $outputname.png introuvable dans le dossier $outputfolder/textures");
+	}
+	$craft->addItem(new Item($outputpath), Craft::CRAFT_SLOT_RESULT);
+
+	$craftname = "craft_" . explode("/", $outputname)[array_key_last(explode("/", $outputname))];
+	$craft->export(dirname(__DIR__) . "/static/craft/" . $craftname . ".png");
 }
