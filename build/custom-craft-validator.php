16bfab726ff3b8ce19a22e05ddabd56a82d47960
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-03-14 14:57:26 +0100

    test amélioration log action

--- build/custom-craft-validator.php
+++ build/custom-craft-validator.php
@@ -8,6 +8,8 @@ if (!is_dir($recipePath)) {
     exit(1);
 }
 
+$hasError = false;
+
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($recipePath, FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_PATHNAME)) as $file) {
     printWarning("find: $file");
     if (substr($file, strlen($file) - 5, 5) !== ".json") {
@@ -17,31 +19,37 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($recipePat
     $data = json_decode(file_get_contents($file), true);
     if ($data === null) {
         printError("invalid json provided");
-        exit(1);
+        $hasError = true;
+        continue;
     }
     printStatement("Valid json format");
     if (!isset($data["input"])) {
         printError("'input' element is missing");
-        exit(1);
+        $hasError = true;
+        continue;
     }
     if (!isset($data["output"])) {
         printError("'output' element is missing");
-        exit(1);
+        $hasError = true;
+        continue;
     }
     if (!isset($data["shape"])) {
         printError("'shape' element is missing");
-        exit(1);
+        $hasError = true;
+        continue;
     }
     for ($i=0; $i < 3; $i++) {
         for ($j = 0;$j < 3;$j++) {
             if (!isset($data["shape"][$i][$j])) {
                 printError("'shape' $i | $j is missing");
-                exit(1);
+                $hasError = true;
+                continue;
             }
             $inputType = $data["shape"][$i][$j];
             if (($inputType !== " " && $inputType !== "") && !isset($data["input"][$inputType])) {
                 printError("'input' name $inputType is missing from input element");
-                exit(1);
+                $hasError = true;
+                continue;
             }
         }
     }
@@ -49,31 +57,43 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($recipePat
     foreach ($data["input"] as $inputType => $dat) {
         if (!isset($dat["folder"])) {
             printError("'folder' element is missing in input");
-            exit(1);
+            $hasError = true;
+            continue;
         }
         if (!isset($dat["name"])) {
             printError("'name' element is missing in input");
-            exit(1);
+            $hasError = true;
+            continue;
         }
         $texturePath = dirname(__DIR__) . "/static/" . $dat["folder"] . "/textures/" . $dat["name"] . ".png";
         if (!is_file($texturePath)) {
             printError("unknow texture, path not found for: $texturePath");
-            exit(1);
+            $hasError = true;
+            continue;
         }
     }
     if (!isset($data["output"][0]["folder"])) {
         printError("'folder' element is missing in output");
-        exit(1);
+        $hasError = true;
+        continue;
     }
     if (!isset($data["output"][0]["name"])) {
         printError("'name' element is missing in output");
-        exit(1);
+        $hasError = true;
+        continue;
     }
     $texturePath = dirname(__DIR__) . "/static/" . $data["output"][0]["folder"] . "/textures/" . $data["output"][0]["name"] . ".png";
     if (!is_file($texturePath)) {
         printError("unknow texture, path not found for: $texturePath");
-        exit(1);
+        $hasError = true;
+        continue;
     }
-    printSuccess(getFilename($file) . " valid");
+    if(!$hasError){
+        printSuccess(getFilename($file) . " valid");
+    }
 }
-printSuccess("craft valid");
+if(!$hasError){
+    printSuccess("craft valid");
+}else{
+    exit(1);
+}
