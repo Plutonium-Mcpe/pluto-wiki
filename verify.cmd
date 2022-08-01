74cca226fad087027bd270a1e8481e6f54514498
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2022-08-01 15:18:49 +0200

    fix duplicata test

--- verify.cmd
+++ verify.cmd
@@ -1,4 +1,4 @@
 php build/file-name-validator.php docs
 php build/file-skeleton-validator.php docs
-php build/file-name-validator.php docs
+php build/file-content-validator.php docs
 php build/custom-craft-validator.php
